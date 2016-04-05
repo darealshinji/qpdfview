795d376a4e4a90bc45a715ce285e2253d00ddfbb
darealshinji <djcj@gmx.de>
2016-04-05 12:42:17 +0200

    push all release tags with one command

--- import-from-launchpad.sh
+++ import-from-launchpad.sh
@@ -9,5 +9,5 @@ bzr fast-export --plain qpdfview | git fast-import
 git checkout -f master 2>/dev/null
 rm -rf qpdfview
 
-#git push origin qpdfview-0.4.16
+#git push origin `git tag | tr '\n' ' '`
 
