724bc44a96068028c8bbe61f76b3c3d5552a7cb5
darealshinji <djcj@gmx.de>
2018-06-02 13:35:27 +0200

    Update import-from-launchpad.sh

--- import-from-launchpad.sh
+++ import-from-launchpad.sh
@@ -12,5 +12,5 @@ bzr fast-export --plain qpdfview | git fast-import
 git checkout -f master 2>/dev/null
 rm -rf qpdfview
 
-#git push origin `git tag | tr '\n' ' '`
+git push origin $(git tag | tr '\n' ' ')
 
