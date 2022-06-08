da97674decb39283eb2fea5f771a4bc4cd25f2eb
darealshinji <djcj@gmx.de>
2022-06-09 01:59:38 +0200

    Update import-from-launchpad.sh

--- import-from-launchpad.sh
+++ import-from-launchpad.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
+set -e
+set -x
 
-#pacman -S bzr bzr-fastimport git
+# required packages: bzr bzr-fastimport git
 
-# bzr-fastimport is not available in 16.04:
-# http://archive.ubuntu.com/ubuntu/pool/universe/b/bzr-fastimport/bzr-fastimport_0.13.0+bzr361-1ubuntu1_all.deb
-
-git clone https://github.com/darealshinji/qpdfview
+git clone --branch master https://github.com/darealshinji/qpdfview
 cd qpdfview
 bzr branch lp:qpdfview
 bzr fast-export --plain qpdfview | git fast-import
 git checkout -f master 2>/dev/null
 rm -rf qpdfview
-
-git push origin master
-git push origin $(git tag | tr '\n' ' ')
-
+#git push origin master
+#git push origin $(git tag | tr '\n' ' ')
